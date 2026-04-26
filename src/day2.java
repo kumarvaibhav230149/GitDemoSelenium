4c5822135d10aa9cb1244ac19ed1a7a34e53e534
Vaibhav <kumar230149@gmail.com>
2026-04-26 17:36:17 +0530

    final commit

---
+++ src/day2.java
@@ -0,0 +1,52 @@
+
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class day2 {
+
+	@Test
+	public void name()
+	{
+		System.out.println();
+	}
+	
+	@BeforeTest
+	public void first1()
+	{
+		System.out.println("Test execution from Before Test");
+	}
+	@Test
+	public void Mobilename1()
+	{
+		System.out.println("Mobile name");
+
+	}
+	
+	@Test
+	public void Mobilecharger2()
+	{
+		System.out.println("Mobile Charger name");
+		
+		System.out.println("Mobile Charger name234");
+		
+		System.out.println("Mobile Charger name6546");
+		
+		System.out.println("Mobile Charger name687654");
+		
+	}
+	
+	@Test
+	public void Laptopcharger3()
+	{
+		System.out.println("Laptop Charger name");
+		
+		System.out.println("Laptop Charger name234");
+		
+		System.out.println("Laptop Charger name6546");
+		
+		System.out.println("Laptop Charger name687654");
+		
+	}
+	
+}
